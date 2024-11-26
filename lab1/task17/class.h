d62e3449d567982ad623aff9928361982c2fb9df
Ivan <81024464+prettyleaf@users.noreply.github.com>
2024-11-26 22:43:49 +0700

    Update class.h

--- lab1/task17/class.h
+++ lab1/task17/class.h
@@ -1,97 +1,31 @@
 #ifndef CLASS_H
 #define CLASS_H
-#include <string>
+
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-class NumberText;
-
 class Money {
-protected:
+private:
     int rubles;
-    int kopeyki;    
+    int kopecks;
+
+    void normalize(); // Приватный метод нормализации
+
 public:
-    void normalize() {
-    if (kopeyki >= 100) {
-        rubles += kopeyki / 100;
-        kopeyki %= 100;
-        } else if (kopeyki < 0) {
-        int deficit_rubles = (-kopeyki + 99) / 100;
-        rubles -= deficit_rubles;
-        kopeyki += deficit_rubles * 100;
-        }
-    }
-    Money (int rubles = 0, int kopeyki = 0) : rubles(rubles), kopeyki(kopeyki) {}
-    Money operator+(const Money& other) const {
-         Money result = Money(rubles + other.rubles, kopeyki + other.kopeyki);
-         result.normalize();
-         return result;
-    }
-    Money operator-(const Money& other) const {
-        Money result = Money(rubles - other.rubles, kopeyki - other.kopeyki);
-        result.normalize();
-        return result;
-    }
-    Money operator/(const Money& other) const {
-        Money result = Money(rubles / other.rubles, kopeyki / other.kopeyki);
-        result.normalize();
-        return result;
-    }
-    Money operator*(double value) const {
-        int total_kopeyki = (rubles * 100 + kopeyki) * value;
-        Money result(total_kopeyki / 100, total_kopeyki % 100);
-        return result;
-    }
-    Money operator/(double value) const {
-        int total_kopeyki = (rubles * 100 + kopeyki) / value;
-        Money result(total_kopeyki / 100, total_kopeyki % 100);
-        result.normalize();
-        return result;
-    }
+    Money(int rub = 0, int kop = 0); // Конструктор
 
+    int getRubles() const; // Геттер для рублей
+    int getKopecks() const; // Геттер для копеек
 
-    friend ostream& operator<<(ostream& os, const Money& nt) {
-    os << "Рублей: " << nt.rubles << ", Копеек: " << nt.kopeyki;
-    return os;
-    }
+    Money operator+(const Money& other) const; // Оператор сложения
+    Money operator-(const Money& other) const; // Оператор вычитания
+    Money operator*(double multiplier) const;  // Оператор умножения
+    Money operator/(double divisor) const;     // Оператор деления на число
+    double operator/(const Money& other) const; // Оператор деления двух сумм
+
+    friend ostream& operator<<(ostream& os, const Money& money); // Вывод
 };
 
-// class Number {
-// protected:
-//     int value;
-// public:
-//     Number(int v = 0) : value(v) {}
-//     Number operator+(const Number& other) const {
-//         return Number(value + other.value);
-//     }
-//     friend class NumberText;
-// };
-
-// class Text {
-// protected:
-//     string text;
-// public:
-//     Text(string t = "") : text(t) {}
-
-//     Text operator+(const Text& other) const {
-//         return Text(text + other.text);
-//     }
-//     friend class NumberText;
-// };
-
-// class NumberText : public Number, public Text {
-// public:
-//     NumberText(int v, string t) : Number(v), Text(t) {}
-//     NumberText operator+(const NumberText& other) const {
-//         Number new_number = Number(value + other.value);
-//         Text new_text = Text(text + other.text);
-//         return NumberText(new_number.value, new_text.text);
-//     }
-//     friend ostream& operator<<(ostream& os, const NumberText& nt) {
-//         os << "Номер: " << nt.value << ", Текст: " << nt.text;
-//         return os;
-//     }
-// };
-
-#endif
+#endif // CLASS_H
